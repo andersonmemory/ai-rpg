6c54910fde80f5d6d0d3a276672332b961070c5b
andersonmemory <andersonleitedesa2100@gmail.com>
2025-12-07 15:28:16 -0300

    feat: uses gemma as the main LLM provider

--- src/main.py
+++ src/main.py
@@ -1,23 +1,22 @@
 from google import genai
+
 from pathlib import Path
 
 from dotenv import load_dotenv
 import os
 
-env_path =Path.cwd().parent.resolve() / ".env"
+env_path = Path.cwd().parent.resolve() / ".env"
 
 load_dotenv(dotenv_path=env_path)
 
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+GEMMA_API_KEY = os.getenv("GEMMA_API_KEY")
 
-
-# The client gets the API key from the environment variable `GEMINI_API_KEY`.
-client = genai.Client(api_key=GEMINI_API_KEY)
-
+# The client gets the API key from the environment variable `GEMMA_API_KEY`.
+client = genai.Client(api_key=GEMMA_API_KEY)
 
 response = client.models.generate_content(
-    model="gemini-2.5-flash", 
-    contents="Explain how AI works in a few words"
+    model="gemma-3-27b-it",
+    contents="Roses are red...",
 )
 
 print(response.text)
