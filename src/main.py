a1710163a436eb6ffcb021ca4f20e491c8fe79ab
andersonmemory <andersonleitedesa2100@gmail.com>
2025-12-07 21:58:51 -0300

    feat: add player 2 AI to the game

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import requests
 import os.path
 from pathlib import Path
+import asyncio
 
 from murf import Murf
 import requests
@@ -30,7 +31,7 @@ gemma_client = genai.Client(api_key=GEMMA_API_KEY)
 murf_client = Murf(api_key=MURF_API_KEY)
 
 
-def main():
+async def main():
 
     if not os.path.exists(history_path):
         print("Não há nenhum arquivo de histórico, criando um.")
@@ -59,7 +60,7 @@ def main():
                 history += f.readline()
 
 
-        prompt = f"""Você é um boneco de jogo de rpg em jogo de aventura. Um mestre está narrando
+        prompt = f"""Você é um boneco de jogo de rpg em jogo de aventura, junto com outro jogador. Um mestre está narrando
         e você só reage aos eventos""" + user_input + """
         """ + """(seja curto, mas nem tanto e lembre-se que é uma fala de um personagem em tom coloquial, 
         mas não faça de propósito nem coloque jargões ou repetições, entre no personagem e reaja de uma forma
@@ -79,17 +80,8 @@ def main():
 
         new_generated_text = response.text
 
-        audio_res = murf_client.text_to_speech.generate(
-            text=new_generated_text,
-            voice_id="Silvio"
-        )
-
-        request = requests.get(audio_res.audio_file)
-
-        with open("file.wav", 'wb') as f:
-            f.write(request.content)
-
-        completed_process = run(split('cvlc file.wav'))
+        # Done: put player speak here
+        await player_speak(1, new_generated_text)
 
         instruction = f"""
             Você é um **Cronista da Aventura** com a tarefa de criar um resumo conciso e completo do histórico de jogo (session history).
@@ -109,7 +101,66 @@ def main():
             **TEXTO COMPLETO A SER RESUMIDO E COMPACTADO:**
             ---
             **Narração do Mestre:** {user_input}
-            **Última Resposta do Jogador:** {new_generated_text}
+            **Última Resposta do Jogador 1:** {new_generated_text}
+            **Última Resposta do Jogador 2:** {new_generated_text}
+            ---
+            """
+
+        response2 = gemma_client.models.generate_content(
+            model="gemma-3-27b-it",
+            contents=instruction,
+        )
+
+        result = response2.text
+        print(result)
+
+        with open("history.txt", 'a') as f:
+            f.write(f"\n{result}")
+
+        prompt2 = f"""Você é um boneco de jogo de rpg em jogo de aventura, junto com outro jogador. Um mestre está narrando
+        e você só reage aos eventos""" + user_input + """
+        """ + """(seja curto, mas nem tanto e lembre-se que é uma fala de um personagem em tom coloquial, 
+        mas não faça de propósito nem coloque jargões ou repetições, entre no personagem e reaja de uma forma
+        como ele realmente iria se sentir nessa situação, tente não falar de si em terceira pessoa ou repetir
+        informações que o mestre diz, fale como se nem estivesse ouvindo o mestre e sim reagindo e que 
+        tudo o que está sendo descrito são na verdade o que está ocorrendo e como você reage a isso)
+
+        aqui está a sequência de eventos do que você ou outros realizaram até então e prossiga a partir
+        disso (se não tiver nada aqui apenas faça o que foi pedido):
+        {history}
+        """
+
+        response = gemma_client.models.generate_content(
+            model="gemma-3-27b-it",
+            contents=prompt,
+        )
+
+        new_generated_text_2 = response.text
+
+        # TODO: AI player 2 
+        await player_speak(2, new_generated_text_2)
+
+
+        instruction = f"""
+            Você é um **Cronista da Aventura** com a tarefa de criar um resumo conciso e completo do histórico de jogo (session history).
+
+            Este resumo servirá como base de memória para a continuidade da narrativa, devendo ser totalmente compreensível para qualquer um que o leia pela primeira vez, permitindo a retomada imediata da história a partir do ponto final.
+
+            **INFORMAÇÕES ESSENCIAIS A INCLUIR NO RESUMO:**
+
+            * **Contexto e Ambiente:** Onde o personagem está, como o local é (descrição, atmosfera - calmo, perigoso, etc.) e como ele chegou ali.
+            * **Personagem:** Identidade, ferimentos recentes, estado de ânimo, habilidades relevantes usadas.
+            * **Inventário/Recursos:** Itens importantes guardados ou usados recentemente.
+            * **Interações:** NPCs encontrados, diálogos cruciais, pedidos de ajuda ou informações.
+            * **Sequência de Ações:** O que foi feito, as últimas ações do personagem e a resposta do ambiente/Mestre a elas.
+
+            **Sintetize o máximo, mantendo apenas o essencial para a continuidade da história.**
+
+            **TEXTO COMPLETO A SER RESUMIDO E COMPACTADO:**
+            ---
+            **Narração do Mestre:** {user_input}
+            **Última Resposta do Jogador 1:** {new_generated_text}
+            **Última Resposta do Jogador 2:** {new_generated_text_2}
             ---
             """
 
@@ -125,6 +176,46 @@ def main():
             f.write(f"\n{result}")
 
 
+async def player_speak(player : int, message : str):
+
+        if player == 1:
+
+            audio_res = murf_client.text_to_speech.generate(
+            text=message,
+            voice_id="Silvio"
+            )
+
+            duration_in_seconds = audio_res.audio_length_in_seconds
+
+            print(f"duração do áudio é: {duration_in_seconds}")
+
+            request = requests.get(audio_res.audio_file)
+
+            with open("file.wav", 'wb') as f:
+                f.write(request.content)
+
+            run(split('cvlc file.wav'))
+            asyncio.sleep(duration_in_seconds)
+
+        elif player == 2:
+            audio_res = murf_client.text_to_speech.generate(
+            text=message,
+            voice_id="Gustavo"
+            )
+
+            duration_in_seconds = audio_res.audio_length_in_seconds
+
+            print(f"duração do áudio é: {duration_in_seconds}")
+
+            request = requests.get(audio_res.audio_file)
+
+            with open("file2.wav", 'wb') as f:
+                f.write(request.content)
+
+            run(split('cvlc file2.wav'))
+            asyncio.sleep(duration_in_seconds)
+
+
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
 
