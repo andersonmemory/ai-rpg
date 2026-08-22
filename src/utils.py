b81f5183bafee60aab4704021a8033ab84b64843
andersonmemory <andersonleitedesa2100@protonmail.com>
2026-08-22 16:21:00 -0300

    feat: add utilities functions for readability

---
+++ src/utils.py
@@ -0,0 +1,44 @@
+import os
+
+from dotenv import load_dotenv
+from google import genai
+
+custom_instructions = """
+    - Answer in plain text, in a continuous line, pure text. Natural way.
+"""
+
+load_dotenv()
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+
+def read(input):
+
+    interaction = client.interactions.create(
+        model="gemma-4-26b-a4b-it", input=f"{custom_instructions} {input}"
+    )
+    # clean_text = re.sub(clean_pattern, "", interaction.output_text, flags=re.M)
+    # print(clean_text)
+    # return clean_text
+    print(interaction.output_text)
+    return interaction.output_text
+
+
+def read_stream(input):
+
+    output_text = []
+
+    stream = client.interactions.create(
+        model="gemma-4-26b-a4b-it",
+        input=f"{custom_instructions} {input}",
+        stream=True,
+    )
+    for event in stream:
+        if event.event_type == "step.delta":
+            if event.delta.type == "text":
+                # clean_text = re.sub(clean_pattern, "", event.delta.text, flags=re.M)
+                print(event.delta.text, end="", flush=True)
+                output_text.append(event.delta.text)
+                # output_text.append(clean_text)
+
+    print()
+    return "".join(output_text)
