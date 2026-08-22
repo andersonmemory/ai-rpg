00fe84fd2b9348591404282d62e3f2587fc7f221
andersonmemory <andersonleitedesa2100@protonmail.com>
2026-08-22 16:44:06 -0300

    feat: implement a basic MVP game loop with runtime memory

--- src/main.py
+++ src/main.py
@@ -5,8 +5,16 @@ from utils import *
 
 def main():
 
+    memory = []
     dm_response = "Você está em 1982, numa prisão. Na sua frente uma pequena serra caseira, na sua esquerda a porta da cela, e na sua direita uma janela com grades de ferro, o que você faz?"
     ai = read_stream(dm_response)
+    memory += [dm_response, ai]
+
+    while True:
+        dm = input("Continue: ")
+        memory.append(dm)
+        ai = read_stream(f"{''.join(memory)} {dm}")
+        memory.append(ai)
 
 
 if __name__ == "__main__":
