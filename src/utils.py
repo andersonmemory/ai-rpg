6112a5f7363ce94dd3ac17b79578a65d79fc5d99
andersonmemory <andersonleitedesa2100@protonmail.com>
2026-08-24 19:57:33 -0300

    feat: implement fully working agent implementation with tool calling

--- src/utils.py
+++ src/utils.py
@@ -8,12 +8,20 @@ import random
 MODEL = "qwen/qwen3.6-27b"
 
 custom_instructions = """
-    - Answer in plain text, in a continuous line, pure text. Natural way.
+    - Answer in plain text, brazilian portuguese, in a continuous line, pure text. Natural way.
     - Few sentences, this is a game.
     - You're receiving the narration from a DM, and you are the player.
     - You have to decide as if you are the character.
     - When asked by the DM roll the die using roll_d6 and say out your result.
     - You're responsible for your own dice rolls.
+
+    What to do when executing a dice roll:
+    > State what value you got and wait for the next response. Its the DM job
+    to narrate what happens next based on your result.
+
+    Example: 
+    DM: Do a test check 
+    AI: It's a 6
 """
 
 load_dotenv()
@@ -22,48 +30,75 @@ client = Groq(
     api_key=os.environ.get("GROQ_API_KEY"),
 )
 
+# shared memory
+messages = [
+    {"role": "system", "content": custom_instructions},
+]
+
 
 # TODO: have your own tools.py file for modularity with some .json loading
 def read_stream_call(input):
     # initialize message
-    messages = [
-        {"role": "system", "content": custom_instructions},
-        {"role": "user", "content": input},
-    ]
+    messages.append(
+        {
+            "role": "user",
+            "content": f" {input}",
+        }
+    )
+
     stream = client.chat.completions.create(
         messages=messages, model=MODEL, tools=[roll], tool_choice="auto", stream=True
     )
 
     # initialize variables
-    final_output = []
+    output = []
     tool_calls = []
+    finish_reason = None
 
     for chunk in stream:
         if chunk.choices[0].delta.content:
             print(chunk.choices[0].delta.content, end="", flush=True)
-            final_output.append(chunk.choices[0].delta.content)
+            output.append(chunk.choices[0].delta.content)
 
         if chunk.choices[0].delta.tool_calls:
-            tool_calls = chunk.choices[0].delta.tool_calls
+            tool_calls.extend(chunk.choices[0].delta.tool_calls)
 
         if chunk.choices[0].finish_reason == "stop":
-            print()
-            return "".join(final_output)
+            finish_reason = "stop"
         elif chunk.choices[0].finish_reason == "tool_calls":
-            messages.extend(execute_tools(tool_calls))
+            finish_reason = "tool_calls"
 
-            stream_call = client.chat.completions.create(
-                messages=messages, model=MODEL, stream=True
-            )
+    if tool_calls:
+        messages.append(
+            {"role": "assistant", "content": "".join(output), "tool_calls": tool_calls}
+        )
+    else:
+        messages.append({"role": "assistant", "content": "".join(output)})
 
-            for chunk in stream_call:
-                if chunk.choices[0].delta.content:
-                    print(chunk.choices[0].delta.content, end="", flush=True)
-                    final_output.append(chunk.choices[0].delta.content)
+    if finish_reason == "stop":
+        print()
+        return "".join(output)
 
-                if chunk.choices[0].finish_reason == "stop":
-                    print()
-                    return "".join(final_output)
+    elif finish_reason == "tool_calls":
+        messages.extend(execute_tools(tool_calls))
+
+        stream_call = client.chat.completions.create(
+            messages=messages,
+            model=MODEL,
+            stream=True,
+        )
+
+        final_output = []
+
+        for chunk in stream_call:
+            if chunk.choices[0].delta.content:
+                print(chunk.choices[0].delta.content, end="", flush=True)
+                final_output.append(chunk.choices[0].delta.content)
+
+            if chunk.choices[0].finish_reason == "stop":
+                print()
+                messages.append({"role": "assistant", "content": "".join(final_output)})
+                return "".join(final_output)
 
 
 # Function definition:
@@ -87,6 +122,9 @@ roll = {
 def execute_tools(tool_calls):
     tools = []
     for tool_call in tool_calls:
+        if not tool_call.function or not tool_call.function.name:
+            continue
+
         id = tool_call.id
         name = tool_call.function.name
         function = tool_map[name]
