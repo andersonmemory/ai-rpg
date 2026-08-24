81c3833e2b5b8d4ab333cf5679d45328f2ee93e1
andersonmemory <andersonleitedesa2100@protonmail.com>
2026-08-24 20:10:12 -0300

    refactor: remove unecessary comments

--- src/main.py
+++ src/main.py
@@ -1,20 +1,15 @@
 import os
-from google import genai
 from utils import *
 
 
 def main():
 
-    memory = []
-    dm_response = "Você está em 1982, numa prisão. Na sua frente uma pequena serra caseira, na sua esquerda a porta da cela, e na sua direita uma janela com grades de ferro, o que você faz?"
-    ai = read_stream(dm_response)
-    memory += [dm_response, ai]
-
+    ai = read_stream_call(
+        "Você está em 1982, numa prisão. Na sua frente uma pequena serra caseira, na sua esquerda a porta da cela, e na sua direita uma janela com grades de ferro, o que você faz?"
+    )
     while True:
-        dm = input("Continue: ")
-        memory.append(dm)
-        ai = read_stream(f"{''.join(memory)} {dm}")
-        memory.append(ai)
+        dm = input("\nDM: ")
+        ai = read_stream_call(dm)
 
 
 if __name__ == "__main__":
