e975bf9a12640814565d83eff3e4cb5c19aef0e9
andersonmemory <andersonleitedesa2100@protonmail.com>
2026-08-24 18:51:09 -0300

    feat: add streaming with tool calling

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,19 @@
 import os
 
+import json
 from dotenv import load_dotenv
 from groq import Groq
 import random
+
+MODEL = "qwen/qwen3.6-27b"
 
 custom_instructions = """
     - Answer in plain text, in a continuous line, pure text. Natural way.
     - Few sentences, this is a game.
     - You're receiving the narration from a DM, and you are the player.
     - You have to decide as if you are the character.
+    - When asked by the DM roll the die using roll_d6 and say out your result.
+    - You're responsible for your own dice rolls.
 """
 
 load_dotenv()
@@ -18,57 +23,8 @@ client = Groq(
 )
 
 
-def read(input):
-
-    chat_completion = client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": f"{custom_instructions} {input}",
-            }
-        ],
-        model="qwen/qwen3.6-27b",
-        reasoning_format="hidden",
-    )
-
-    print(chat_completion.choices[0].message.content)
-    return chat_completion.choices[0].message.content
-
-
-def read_stream(input):
-
-    output_text = []
-    stream = client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": f"{custom_instructions} {input}",
-            }
-        ],
-        model="qwen/qwen3.6-27b",
-        stream=True,
-        reasoning_format="hidden",
-    )
-
-    for chunk in stream:
-        text = chunk.choices[0].delta.content
-        if text:
-            print(text, end="", flush=True)
-            output_text.append(text)
-
-    print()
-    return "".join(output_text)
-
-
 # TODO: have your own tools.py file for modularity with some .json loading
 # TODO: executes tool call and prints the content once it's finished
-def read_stream_call():
-    # TODO: initialize message
-    # TODO: tool schema
-    # returns the final content
-    pass
-
-
 # TODO: executes tool call and streams the content
 def read_stream_call(input):
     # initialize message
@@ -76,17 +32,52 @@ def read_stream_call(input):
         {"role": "system", "content": custom_instructions},
         {"role": "user", "content": input},
     ]
+    stream = client.chat.completions.create(
+        messages=messages, model=MODEL, tools=[roll], tool_choice="auto", stream=True
+    )
 
-    # return printed chunks with flush enabled
-    # return list containing all chunks together
-    pass
+    # initialize variables
+    final_output = []
+    tool_calls = []
+
+    for chunk in stream:
+        if chunk.choices[0].delta.content:
+            print(chunk.choices[0].delta.content, end="", flush=True)
+            final_output.append(chunk.choices[0].delta.content)
+
+        if chunk.choices[0].delta.tool_calls:
+            tool_calls = chunk.choices[0].delta.tool_calls
+
+        if chunk.choices[0].finish_reason == "stop":
+            print()
+            return "".join(final_output)
+        elif chunk.choices[0].finish_reason == "tool_calls":
+            messages.extend(execute_tools(tool_calls))
+
+            stream_call = client.chat.completions.create(
+                messages=messages, model=MODEL, stream=True
+            )
+
+            for chunk in stream_call:
+                if chunk.choices[0].delta.content:
+                    print(chunk.choices[0].delta.content, end="", flush=True)
+                    final_output.append(chunk.choices[0].delta.content)
+
+                if chunk.choices[0].finish_reason == "stop":
+                    print()
+                    return "".join(final_output)
+
+
+# Function definition:
+def roll_d6():
+    return json.dumps(random.randint(1, 6))
 
 
 # Tool map:
-available_functions = {"roll_d6": roll_d6}
+tool_map = {"roll_d6": roll_d6}
 
 # Tool definitions:
-roll_d6 = {
+roll = {
     "type": "function",
     "function": {
         "name": "roll_d6",
@@ -95,6 +86,14 @@ roll_d6 = {
 }
 
 
-# Function definition:
-def roll_d6():
-    return random.randint(1, 6)
+def execute_tools(tool_calls):
+    tools = []
+    for tool_call in tool_calls:
+        id = tool_call.id
+        name = tool_call.function.name
+        function = tool_map[name]
+        result = function()
+        tools.append(
+            {"role": "tool", "content": result, "tool_call_id": id, "name": name}
+        )
+    return tools
