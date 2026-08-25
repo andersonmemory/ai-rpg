ed853941c3a8bfe441990f3ad6a1dc638069a51d
andersonmemory <andersonleitedesa2100@protonmail.com>
2026-08-25 12:43:44 -0300

    feat: remove tool calling to increase speed

--- src/main.py
+++ src/main.py
@@ -4,12 +4,12 @@ from utils import *
 
 def main():
 
-    ai = read_stream_call(
+    ai = read_stream(
         "Você está em 1982, numa prisão. Na sua frente uma pequena serra caseira, na sua esquerda a porta da cela, e na sua direita uma janela com grades de ferro, o que você faz?"
     )
     while True:
         dm = input("\nDM: ")
-        ai = read_stream_call(dm)
+        ai = read_stream(dm)
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -12,16 +12,6 @@ custom_instructions = """
     - Few sentences, this is a game.
     - You're receiving the narration from a DM, and you are the player.
     - You have to decide as if you are the character.
-    - When asked by the DM roll the die using roll_d6 and say out your result.
-    - You're responsible for your own dice rolls.
-
-    What to do when executing a dice roll:
-    > State what value you got and wait for the next response. Its the DM job
-    to narrate what happens next based on your result.
-
-    Example: 
-    DM: Do a test check 
-    AI: It's a 6
 """
 
 load_dotenv()
@@ -36,8 +26,10 @@ messages = [
 ]
 
 
-# TODO: have your own tools.py file for modularity with some .json loading
-def read_stream_call(input):
+def read_stream(input):
+
+    output = []
+
     # initialize message
     messages.append(
         {
@@ -49,90 +41,22 @@ def read_stream_call(input):
     stream = client.chat.completions.create(
         messages=messages,
         model=MODEL,
-        tools=[roll],
-        tool_choice="auto",
         stream=True,
         reasoning_format="hidden",
     )
 
-    # initialize variables
-    output = []
-    tool_calls = []
     finish_reason = None
 
     for chunk in stream:
         if chunk.choices[0].delta.content:
-            print(chunk.choices[0].delta.content, end="", flush=True)
             output.append(chunk.choices[0].delta.content)
-
-        if chunk.choices[0].delta.tool_calls:
-            tool_calls.extend(chunk.choices[0].delta.tool_calls)
-
+            print(chunk.choices[0].delta.content, end="", flush=True)
         if chunk.choices[0].finish_reason == "stop":
             finish_reason = "stop"
-        elif chunk.choices[0].finish_reason == "tool_calls":
-            finish_reason = "tool_calls"
-
-    if tool_calls:
-        messages.append(
-            {"role": "assistant", "content": "".join(output), "tool_calls": tool_calls}
-        )
-    else:
-        messages.append({"role": "assistant", "content": "".join(output)})
 
     if finish_reason == "stop":
         print()
+        messages.append({"role": "assistant", "content": "".join(output)})
         return "".join(output)
-
-    elif finish_reason == "tool_calls":
-        messages.extend(execute_tools(tool_calls))
-
-        stream_call = client.chat.completions.create(
-            messages=messages, model=MODEL, stream=True, reasoning_format="hidden"
-        )
-
-        final_output = []
-
-        for chunk in stream_call:
-            if chunk.choices[0].delta.content:
-                print(chunk.choices[0].delta.content, end="", flush=True)
-                final_output.append(chunk.choices[0].delta.content)
-
-            if chunk.choices[0].finish_reason == "stop":
-                print()
-                messages.append({"role": "assistant", "content": "".join(final_output)})
-                return "".join(final_output)
-
-
-# Function definition:
-def roll_d6():
-    return json.dumps(random.randint(1, 6))
-
-
-# Tool map:
-tool_map = {"roll_d6": roll_d6}
-
-# Tool definitions:
-roll = {
-    "type": "function",
-    "function": {
-        "name": "roll_d6",
-        "description": "rolls a d6 dice, only use if it is asked to perform a test",
-    },
-}
-
-
-def execute_tools(tool_calls):
-    tools = []
-    for tool_call in tool_calls:
-        if not tool_call.function or not tool_call.function.name:
-            continue
-
-        id = tool_call.id
-        name = tool_call.function.name
-        function = tool_map[name]
-        result = function()
-        tools.append(
-            {"role": "tool", "content": result, "tool_call_id": id, "name": name}
-        )
-    return tools
+    else:
+        print(f"ERROR: Unknown finish_reason: {finish_reason}")
